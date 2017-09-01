fc1b2e6b58ee09bbd0a3f49bdba123ba9f447b91
Jonas Hoersch <jonas@chaoflow.net>
2017-09-02 01:54:59 +0200

    madd: Always reindexes

--- scripts/_helpers.py
+++ scripts/_helpers.py
@@ -11,23 +11,20 @@ def madd(n, component, name=None, index=None, **kwargs):
 
     static = {}; series = {}
     for k, v in iteritems(kwargs):
-        if isinstance(v, pd.Series) and isinstance(v.index, pd.DatetimeIndex):
-            v = pd.DataFrame(np.repeat(v.values[:,np.newaxis], len(new_index), axis=1),
-                             index=v.index, columns=new_index)
-            series[k] = v
-        elif isinstance(v, pd.DataFrame) and isinstance(v.index, pd.DatetimeIndex):
-            v = pd.DataFrame(v)
-            if len(v.columns.difference(index)) == 0:
-                v = v.reindex(columns=index)
+        if isinstance(v, pd.Series):
+            if len(v.index.intersection(index)) > 0:
+                static[k] = v.reindex(index).values
+            else:
+                v = pd.DataFrame(np.repeat(v.values[:,np.newaxis], len(new_index), axis=1),
+                                index=v.index, columns=new_index)
+                series[k] = v
+        elif isinstance(v, pd.DataFrame):
+            v = pd.DataFrame(v).reindex(columns=index)
             v.columns = new_index
             series[k] = v
         elif isinstance(v, np.ndarray) and v.shape == (len(n.snapshots), len(new_index)):
             series[k] = pd.DataFrame(v, index=n.snapshots, columns=new_index)
         else:
-            if isinstance(v, pd.Series):
-                if len(v.index.difference(index)) == 0:
-                    v = v.reindex(index)
-                v = v.values
             static[k] = v
 
     n.import_components_from_dataframe(pd.DataFrame(static, index=new_index), component)
