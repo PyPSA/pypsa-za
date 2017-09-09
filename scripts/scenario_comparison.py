4ed3fc2e8ba007f81d2a8f066e06fb6ea54a1fbf
Jonas Hoersch <jonas@chaoflow.net>
2017-09-09 10:33:47 +0200

    scenario_comparison: Use dedent to clean up

--- scripts/scenario_comparison.py
+++ scripts/scenario_comparison.py
@@ -2,18 +2,19 @@ import os
 from itertools import product
 from snakemake.utils import report
 from snakemake.report import data_uri
+from textwrap import dedent
 
 param = snakemake.wildcards.param
 plot_dir = snakemake.params.plot_dir
 
-rest = '''
-===========================
-Comparison of param {param}
-===========================
+text = dedent('''
+    ===========================
+    Comparison of param {param}
+    ===========================
 
-.. contents:: Table of Contents
+    .. contents:: Table of Contents
 
-'''.format(param=param)
+    ''').format(param=param)
 
 scenarios = snakemake.config['scenario'].copy()
 param_values = scenarios.pop(snakemake.wildcards.param)
@@ -24,27 +25,26 @@ links = {}
 for vals in product(*scenarios.values()):
     sc = dict(zip(scenarios.keys(), vals))
     headline = ", ".join("{}={}".format(k,v) for k, v in sc.items())
-    rest += headline + "\n" + '-' * len(headline) + "\n\n"
+    text += headline + "\n" + '-' * len(headline) + "\n\n"
     for p in param_values:
         sc[param] = p
         fn = tmpl.format(**sc)
-        links[fn] = [os.path.join(plot_dir, fn + '.pdf')]
-        rest += '''
-.. figure:: {data}
-   :scale: 50 %
+        #links[fn] = [os.path.join(plot_dir, fn + '.pdf')]
+        text += dedent('''
+            .. figure:: {data}
+               :scale: 50 %
 
-   {param} = {value}
+               {param} = {value}
 
-   {link}_
+        ''').format(param=param, value=p, link=fn,
+                    data=data_uri(os.path.join(plot_dir, fn + '.png')))
 
-'''.format(param=param, value=p,
-           link=fn, data=data_uri(os.path.join(plot_dir, fn + '.png')))
+# text += dedent('''
 
-rest += '''
+#     Attachments
+#     -----------
+#     ''')
 
-Attachments
------------
-'''
-                # rest += '{} = {}: {}_\n\n.. image:: {}\n\n'.format(wildcards.param, p, fn, data_uri('results/plots/' + fn + '.png'))
+# text += '{} = {}: {}_\n\n.. image:: {}\n\n'.format(wildcards.param, p, fn, data_uri('results/plots/' + fn + '.png'))
 
-report(text=rest, path=snakemake.output.html, stylesheet='report.css', **links)
+report(text=text, path=snakemake.output.html, stylesheet='report.css', **links)
