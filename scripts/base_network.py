df26d36244c7f31c1e3762d4d7352a38b45bdebe
Jonas Hoersch <jonas@chaoflow.net>
2017-09-16 10:18:14 +0200

    Refactor line build up

--- scripts/base_network.py
+++ scripts/base_network.py
@@ -51,8 +51,9 @@ def base_network():
 
 
     ## Build pypsa network
-    v_nom = 380.
-    line_type = 'Al/St 240/40 4-bundle 380.0'
+    line_config = snakemake.config['lines']
+    v_nom = line_config['v_nom']
+    line_type = line_config['type']
 
     n = pypsa.Network()
     n.name = 'PyPSA-ZA'
@@ -75,23 +76,21 @@ def base_network():
     lines = pd.DataFrame(edges_between_touching_regions(regions), columns=['bus0', 'bus1'])
 
     discountrate = snakemake.config['costs']['discountrate']
-    line_costs = snakemake.config['costs']['line']
     def asarray(x): return np.asarray(list(map(np.asarray, x)))
     lines['length'] = haversine(asarray(lines.bus0.map(centroids)),
-                                asarray(lines.bus1.map(centroids))) * line_costs['length_factor']
-    lines['capital_cost'] = ((annuity(line_costs['lifetime'], discountrate) +
-                              line_costs.get('fom', 0)) * line_costs['overnight'] *
-                             lines['length'] / line_costs['s_nom_factor'])
+                                asarray(lines.bus1.map(centroids))) * line_config['length_factor']
+    lines['capital_cost'] = ((annuity(line_config['lifetime'], discountrate) +
+                              line_config.get('fom', 0)) * line_config['overnight_cost'] *
+                             lines['length'] / line_config['s_nom_factor'])
 
-    num_parallel = (pd.read_csv(snakemake.input.num_lines, index_col=0)
-                    .set_index(['bus0', 'bus1'])
-                    .eval('num_parallel_275 * (275/380)**2 + '
-                          'num_parallel_400 * (400/380)**2 + '
-                          'num_parallel_765 * (765/380)**2'))
+    num_lines = pd.read_csv(snakemake.input.num_lines, index_col=0).set_index(['bus0', 'bus1'])
+    num_parallel = sum(num_lines['num_parallel_{}'.format(int(v))] * (v/v_nom)**2
+                       for v in (275, 400, 765))
+
     lines = (lines
-            .join(num_parallel.rename('num_parallel'), on=['bus0', 'bus1'])
-            .join(num_parallel.rename("num_parallel_i"), on=['bus1', 'bus0']))
-    lines['num_parallel'] = line_costs['s_nom_factor'] * lines['num_parallel'].fillna(lines.pop('num_parallel_i'))
+             .join(num_parallel.rename('num_parallel'), on=['bus0', 'bus1'])
+             .join(num_parallel.rename("num_parallel_i"), on=['bus1', 'bus0']))
+    lines['num_parallel'] = line_config['s_nom_factor'] * lines['num_parallel'].fillna(lines.pop('num_parallel_i'))
     lines['capacity'] = np.sqrt(3)*v_nom*n.line_types.loc[line_type, 'i_nom']*lines.num_parallel
 
     if 'T' in snakemake.wildcards.opts.split('-'):
